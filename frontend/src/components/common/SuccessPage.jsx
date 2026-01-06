41752fbae65da843ae28d99680c64d342e698bcf
Despoina Kampioti <deppykamp@gmail.com>
2026-01-06 22:35:06 +0200

    component for Success Actions

--- frontend/src/components/common/SuccessPage.jsx
+++ frontend/src/components/common/SuccessPage.jsx
@@ -10,12 +10,10 @@ const SuccessPage = ({
   buttonText = 'Επιστροφή',
   onButtonClick,
   iconColor = '#FCA47C',
-  iconBgColor = '#FFF4ED',
-  breadcrumbs = [],
-  pageTitle = ''
+  iconBgColor = '#ffd8c6'
 }) => {
   return (
-    <PageLayout title={pageTitle} breadcrumbs={breadcrumbs}>
+    <PageLayout>
       <div className="success-page">
         <div className="success-page__content">
           <div 
@@ -26,53 +24,7 @@ const SuccessPage = ({
               color: iconColor
             }}
           >
-            <Icon size={54} />
-          </div>
-          <h1 className="success-page__title">{title}</h1>
-          <p className="success-page__description">{description}</p>
-          <button 
-            className="success-page__btn"
-            onClick={onButtonClick}
-          >
-            {buttonText}
-          </button>
-        </div>
-      </div>
-    </PageLayout>
-  );
-};
-
-export default SuccessPage;
-
-import React from 'react';
-import { UserRound } from 'lucide-react';
-import PageLayout from '../global/layout/PageLayout';
-import './SuccessPage.css';
-
-const SuccessPage = ({ 
-  icon: Icon = UserRound, 
-  title = 'Επιτυχία!',
-  description = 'Η ενέργεια ολοκληρώθηκε με επιτυχία.',
-  buttonText = 'Επιστροφή',
-  onButtonClick,
-  iconColor = '#FCA47C',
-  iconBgColor = '#FFF4ED',
-  breadcrumbs = [],
-  pageTitle = ''
-}) => {
-  return (
-    <PageLayout title={pageTitle} breadcrumbs={breadcrumbs}>
-      <div className="success-page">
-        <div className="success-page__content">
-          <div 
-            className="success-page__icon"
-            style={{ 
-              backgroundColor: iconBgColor,
-              borderColor: iconColor,
-              color: iconColor
-            }}
-          >
-            <Icon size={54} />
+            <Icon size={64} />
           </div>
           <h1 className="success-page__title">{title}</h1>
           <p className="success-page__description">{description}</p>
--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -11,7 +11,6 @@ export const ROUTES = {
   information: '/information',
   confirmation: '/confirmation',
   success: '/success',
-  confirm: '/confirm',
   citizen: {
     lostPets: '/citizen/lost-pets',
     foundPetForm: '/citizen/found-pet',
